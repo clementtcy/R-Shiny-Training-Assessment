8639f0795cd97bd5da10a052ed3ebda84aec7802
clementtcy <clement.tan@n-actuarial.com>
2023-04-07 14:09:19 +0800

    testing output

--- app.R
+++ app.R
@@ -1,7 +1,18 @@
 library(shiny)
 ui <- fluidPage(
-  "Hello, world!"
+  selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
+  verbatimTextOutput("summary"),
+  tableOutput("table")
 )
 server <- function(input, output, session) {
+  output$summary <- renderPrint({
+    dataset <- get(input$dataset, "package:datasets")
+    summary(dataset)
+  })
+  
+  output$table <- renderTable({
+    dataset <- get(input$dataset, "package:datasets")
+    dataset
+  })
 }
 shinyApp(ui, server)
